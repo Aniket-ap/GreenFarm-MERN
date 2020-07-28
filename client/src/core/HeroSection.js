92bf7bef9b9a42880e0459d5d829d7eab4b1bf14
Aniket Pradhan <aniket_pro@outlook.com>
2020-07-28 16:09:59 +0530

    updated

--- client/src/core/HeroSection.js
+++ client/src/core/HeroSection.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import { UncontrolledCarousel } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-const items = [
-  {
-    src: 'https://github.com/Aniket-ap/GreenFarm-MERN/blob/master/client/src/images/bgn1.gif?raw=true',
-    altText: 'Slide 1',
-    caption: '#greenfarm',
-    header: 'Fresh and only the Finest produce on your tables',
-    key: '1'
-  },
-  {
-    src: 'https://github.com/Aniket-ap/GreenFarm-MERN/blob/master/client/src/images/bgn2.gif?raw=true',
-    altText: 'Slide 2',
-    caption: '#greenfarm',
-    header: 'Fresh and only the Finest produce on your tables',
-    key: '2'
-  }
-];
+import B1 from '../images/bananac.jpg'
+import B2 from '../images/applec.jpg'
+import B3 from '../images/promganate.jpg'
 
-const HeroSection = () => <UncontrolledCarousel items={items} />;
+
+const HeroSection = () => {
+  return(
+    <div>
+      <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img className="d-block w-100" src={B1} alt="First slide"/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src={B2} alt="Second slide"/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src={B3} alt="Third slide"/>
+    </div>
+  </div>
+  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
+  </a>
+  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
+  </a>
+</div>
+    </div>
+  )
+}
 
 export default HeroSection;
 
